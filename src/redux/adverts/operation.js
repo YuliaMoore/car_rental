ed85d62ecd0e5397c784c7543b300acd6c10e230
Yulia Tkach <115689321+YuliaMoore@users.noreply.github.com>
2023-11-12 23:07:22 +0200

    Add files via upload

---
+++ src/redux/adverts/operation.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseUrl = "https://64bc43077b33a35a444727f9.mockapi.io";
+
+export const fetchCars = createAsyncThunk(
+  "advert/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("./adverts");
+      console.log(fetchCars);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
